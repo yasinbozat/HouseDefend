6fefe28b72bc47d6233d3a8960f265cc9c316110
Yasin Bozat <37029016+yasinbozat@users.noreply.github.com>
2020-10-29 17:53:08 +0300

    Sa AS

--- Assets/_HouseDefend/Scripts/ZombieHealthBar.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class ZombieHealthBar : MonoBehaviour
-{
-    public static float health=1f;
-}
